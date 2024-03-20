f8bf7effa989632828c460e3eb4b3eba47e228e3
johnlstewart <john90stewart@protonmail.com>
2024-03-20 12:52:21 -0600

    bro code java

---
+++ bro code/index.js
@@ -0,0 +1,18 @@
+// Javascript
+// A web-based programming language:
+// Add interactive behavior to webpages
+
+console.log('I like Lasagna');
+console.log("It's really good!");
+
+window.alert("I REALLY LOVE LASAGNA!");
+
+// This is a comment
+
+/*
+    This
+    Is
+    A
+    Multi-line
+    Comment
+*/
