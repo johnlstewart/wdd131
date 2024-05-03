94613b04d79c37557f23d2198322b563215a7cce
JohnLStewart <john90stewart@protonmail.com>
2024-05-02 18:05:59 -0600

    DOM basics

---
+++ week02/dom-basics.js
@@ -0,0 +1,25 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+
+// New Image
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "https://picsum.photos/200");
+newImage.setAttribute("alt", "Random image from picsum.");
+document.body.appendChild(newImage);
+
+// Inserting an entirely new string
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+// Creating a new section element
+const newSection = document.createElement("section");
+const newH2 = document.createElement("h2");
+newH2.innerText = "DOM Basics";
+newSection.appendChild(newH2);
+const newP = document.createElement("p");
+newP.innerText = "This was added through Javascript";
+newSection.appendChild(newP);
+
+document.body.appendChild(newSection);
